567145173f5b71ace73cc062c75fbd2860b62fae
Klaus <klaus.accso@ard.de>
2026-03-18 02:05:00 +0100

    Barcodes are scannable

--- app/src/main/java/com/example/digitalstorageroom/scanner/CameraScreen.kt
+++ app/src/main/java/com/example/digitalstorageroom/scanner/CameraScreen.kt
@@ -1,12 +1,22 @@
 package com.example.digitalstorageroom.scanner
 
 import android.content.Context
+import android.util.Log
+import android.util.Size
 import androidx.camera.compose.CameraXViewfinder
 import androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.core.SurfaceRequest
+import androidx.camera.core.UseCase
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.lifecycle.awaitInstance
+import androidx.camera.view.CameraController
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -32,9 +42,19 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -50,7 +70,10 @@ fun CameraScreen(
         )
     } else {
         Column(
-            modifier = modifier.fillMaxSize().wrapContentSize().widthIn(max = 480.dp),
+            modifier = modifier
+                .fillMaxSize()
+                .wrapContentSize()
+                .widthIn(max = 480.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
@@ -96,7 +119,59 @@ fun CameraContent(
     }
 }
 
+class BarcodeScanner {
 
+    private val options = BarcodeScannerOptions
+        .Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_EAN_8,
+            Barcode.FORMAT_EAN_13
+        )
+        .build()
+
+    private val scanner = BarcodeScanning.getClient(options)
+
+    //TODO: Only scan if barcode is visible
+
+    @androidx.annotation.OptIn(ExperimentalGetImage::class)
+    suspend fun scan(imageProxy: ImageProxy) {
+        Log.i("INFO", "Analyzing image")
+        val image = imageProxy.image ?: return
+        val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+
+        imageProxy.use {
+            scanner.process(InputImage.fromMediaImage(image, rotationDegrees))
+                .addOnSuccessListener { barcodes ->
+                    barcodes
+                        .also {
+                            Log.i("INFO", "Barcodes found: ${it.map({ barcode -> barcode.rawValue }).joinToString(", ")}")
+                        }
+                        // The barcode with the biggest bounding box is deemed to be the one focused by the user
+                        .maxWith { barcode1, barcode2 ->
+                        barcode1.boundingBox!!.width() * barcode1.boundingBox!!.height() -
+                                barcode2.boundingBox!!.width() * barcode2.boundingBox!!.height()
+                    }.let { barcode ->
+                        Log.i("INFO", "Barcode found: ${barcode.rawValue}")
+                    }
+                }
+        }
+
+    }
+}
+
+class CodeAnalyzer : ImageAnalysis.Analyzer {
+
+    val barcodeScanner = BarcodeScanner()
+
+    @androidx.annotation.OptIn(ExperimentalGetImage::class)
+    override fun analyze(imageProxy: ImageProxy) {
+        /*Log.i("INFO", "Analyzing image")
+        CoroutineScope(Dispatchers.Default).launch {
+            barcodeScanner.scan(imageProxy)
+        }
+        imageProxy.close()*/
+    }
+}
 
 class CameraViewModel : ViewModel() {
     private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
@@ -111,17 +186,39 @@ class CameraViewModel : ViewModel() {
             }
         }
 
-    suspend fun bindToCamera(appContext: Context, lifecycleOwner: LifecycleOwner) {
+    //TODO Patrick: cleanup this class. CameraViewModel should be decoupled from analyzer
+    // so that the we can provide different analyzers
+    private val analysisExecutor = Executors.newSingleThreadExecutor()
+
+    private val analyzer = ImageAnalysis.Builder()
+        .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+        .build()
+        .also { it.setAnalyzer(analysisExecutor, CodeAnalyzer()) }
+
+    private val scanner = BarcodeScanner()
+    val imageCapture = ImageCapture
+        .Builder()
+        .build()
+
+    suspend fun bindToCamera(
+        appContext: Context,
+        lifecycleOwner: LifecycleOwner,
+        vararg useCases: UseCase
+    ) {
         val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
+
 
         // unbindAll before binding to ensure a clean state and avoid TimeoutException
         processCameraProvider.unbindAll()
-        
+
         try {
             processCameraProvider.bindToLifecycle(
-                lifecycleOwner, 
+                lifecycleOwner,
                 DEFAULT_BACK_CAMERA,
-                cameraPreview
+                cameraPreview,
+                imageCapture,
+                //analyzer,
+                *useCases
             )
             awaitCancellation()
         } finally {
@@ -131,5 +228,25 @@ class CameraViewModel : ViewModel() {
 
     suspend fun takePhoto() {
         println("INFO: I have taken a photo!!")
+        imageCapture.takePicture(
+            analysisExecutor,
+            object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    Log.i("INFO", "Image captured")
+                    CoroutineScope(Dispatchers.IO).launch {
+                        scanner.scan(image)
+                    }
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e("ERROR", "Photo capture failed: ${exception.message}", exception)
+                }
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        // Shut down the executor to prevent memory leaks
+        analysisExecutor.shutdown()
     }
 }
